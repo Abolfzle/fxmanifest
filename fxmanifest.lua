0d94cecf8b3e1fc7993b8cde82dd0a7be64426e8
spit <123283501+Abolfzle@users.noreply.github.com>
2023-11-20 02:03:56 +0330

    hud_overland_dump_spit
    ---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord

---
+++ fxmanifest.lua
@@ -0,0 +1,50 @@
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page {
+	'ui/index.html'
+}
+
+server_scripts {
+	'server/main.lua',
+}
+client_scripts {
+	'client/main.lua'
+}
+files {
+	"ui/index.html",
+	"ui/assets/css/*.css",
+	"ui/assets/js/*.js",
+	"ui/assets/imgs/*.svg",
+	"ui/assets/imgs/jobs/*.svg",
+	"ui/assets/imgs/*.png",
+	"ui/assets/fonts/*.ttf",
+}
+
+
+
+
+
+
+
+
+--------- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+---- spit developer  # spit    https://discord.gg/rVcdM3hPPv  discord
+
+
+
+
+
+---spit developer 
